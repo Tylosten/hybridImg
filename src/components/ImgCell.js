b23d364fe8d8465e35711a109d239f4130a61084
Tylosten <36881448+Tylosten@users.noreply.github.com>
2020-04-13 13:23:28 +0200

    Adding hybrid management in server side and fetching it from ImgCell.

--- src/components/ImgCell.js
+++ src/components/ImgCell.js
@@ -1,12 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { v4 as uuid } from 'uuid';
 import ImgUpload from './ImgUpload';
+const url = 'http://localhost:4242';
 
 const ImgCell = props => {
+  const [author, setAuthor] = useState('Mag');
   const [imgUrl, setImgUrl] = useState('');
+  const [tags, setTags] = useState([props.col, props.line]);
+  const [hybridId, setHybridId] = useState();
 
-  const onImgUpload = imgFile => {
+  const loadHybrid = async () => {
+    const res = await axios.get(
+      url + `/hybrids/find?author=${author}&tags=${tags}`
+    );
+    const hybrid = res.data[0];
+    if (hybrid) {
+      setImgUrl(hybrid.url);
+      setHybridId(hybrid.id);
+    }
+  };
+
+  useEffect(() => {
+    loadHybrid();
+  }, []);
+
+  const createHybrid = async newUrl => {
+    const hybrid = {
+      id: uuid(),
+      author,
+      url: newUrl,
+      tags,
+    };
+    const res = await axios.post('/hybrids/new', { hybrid });
+    console.info('Created hybrid in db', res);
+  };
+
+  const updateHybrid = async newUrl => {
+    const res = await axios.post('/hybrids/update', {
+      hybrid: {
+        id: hybridId,
+        author,
+        url: newUrl,
+        tags,
+      },
+    });
+    console.info('Updated hybrid in db', res);
+  };
+
+  const onImgUpload = async imgFile => {
     const newUrl = './Images/' + imgFile.name;
     setImgUrl(newUrl);
+    hybridId ? updateHybrid(newUrl) : createHybrid(newUrl);
   };
 
   const deleteImg = () => {};
--- src/server/server.js
+++ src/server/server.js
@@ -2,8 +2,10 @@ import fs from 'fs';
 import path from 'path';
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import morgan from 'morgan';
 import serialize from 'serialize-javascript';
+import { connectDB } from './connect-db';
 
 import config from 'server/config';
 import { serverRenderer } from 'renderers/server';
@@ -13,6 +15,8 @@ app.enable('trust proxy');
 app.use(morgan('common'));
 
 app.use(express.static('public'));
+
+app.use(cors());
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -34,6 +38,81 @@ app.get('/', async (req, res) => {
     console.error(err);
     res.status(500).send('Server error');
   }
+});
+
+export const findAllHybrids = async () => {
+  const db = await connectDB();
+  return db
+    .collection('hybrids')
+    .find()
+    .toArray();
+};
+app.get('/hybrids/findAll', async (req, res) => {
+  const hybrids = await findAllHybrids();
+  res.status(200).json(hybrids);
+});
+
+export const findHybrids = async (author, tags) => {
+  const search = {};
+  if (author) {
+    search.author = author;
+  }
+  if (tags) {
+    search.tags = { $all: (tags = tags.split(',')) };
+  }
+  const db = await connectDB();
+  return db
+    .collection('hybrids')
+    .find(search)
+    .toArray();
+};
+app.get('/hybrids/find', async (req, res) => {
+  const { author, tags } = req.query;
+  const hybrids = await findHybrids(author, tags);
+  res.status(200).json(hybrids);
+});
+
+export const addNewHybrid = async hybrid => {
+  const db = await connectDB();
+  const collection = db.collection('hybrids');
+  await collection.insertOne(hybrid);
+};
+app.post('/hybrids/new', async (req, res) => {
+  const hybrid = req.body.hybrid;
+  await addNewHybrid(hybrid);
+  res.status(200).send();
+});
+
+export const saveHybrid = async hybrid => {
+  const db = await connectDB();
+  const collection = db.collection('hybrids');
+  await collection.save(hybrid);
+};
+app.post('/hybrids/save', async (req, res) => {
+  const hybrid = req.body.hybrid;
+  await saveHybrid(hybrid);
+  res.status(200).send();
+});
+
+export const updateHybrid = async hybrid => {
+  const { id, tags, url, author } = hybrid;
+  const db = await connectDB();
+  const collection = db.collection('hybrids');
+
+  if (author) {
+    await collection.updateOne({ id }, { $set: { author } });
+  }
+  if (tags) {
+    await collection.updateOne({ id }, { $set: { tags } });
+  }
+  if (url) {
+    await collection.updateOne({ id }, { $set: { url } });
+  }
+};
+app.post('/hybrids/update', async (req, res) => {
+  const hybrid = req.body.hybrid;
+  await updateHybrid(hybrid);
+  res.status(200).send();
 });
 
 app.listen(config.port, config.host, () => {
