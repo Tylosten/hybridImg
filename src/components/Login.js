fe001364c01a3f128ead6939bb7ff9a040b9c4df
Tylosten <36881448+Tylosten@users.noreply.github.com>
2020-04-21 10:43:42 +0200

    Loggin in redirects to home page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Redirect, Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import { Form, Button, Columns, Box } from 'react-bulma-components';
 const { Field, Label, Input, Control, Help } = Form;
 const { Column } = Columns;
@@ -7,78 +7,79 @@ const { Column } = Columns;
 import StoreProvider from '../store/StoreProvider';
 import { login } from '../store/StoreActions';
 
-const Login = ({ dispatchToStore }) => {
+const Login = ({ authenticated, dispatchToStore }) => {
   const [username, setUserName] = useState();
   const [password, setPassword] = useState();
 
   const onSubmit = async e => {
     e.preventDefault();
     await dispatchToStore(login(username, password));
-
-    return <Redirect to="/home" />;
   };
 
   return (
-    <Columns className="is-centered">
-      <Column className="is-narrow">
-        <Box style={{ marginTop: '1.5rem' }}>
-          <form>
-            <Field>
-              <Label>Nom</Label>
-              <Control>
-                <Input
-                  placeholder="Votre nom"
-                  value={username}
-                  onChange={e => setUserName(e.target.value)}
-                  required
-                />
-                <Help
-                  color="danger"
-                  style={{ display: username ? 'none' : 'default' }}
-                >
-                  Ce champ est requis
-                </Help>
-              </Control>
-            </Field>
-            <Field>
-              <Label>Mot de passe</Label>
-              <Control>
-                <Input
-                  type="password"
-                  placeholder="Votre mot de passe"
-                  value={password}
-                  onChange={e => setPassword(e.target.value)}
-                  required
-                />
-                <Help
-                  color="danger"
-                  style={{ display: password ? 'none' : 'default' }}
-                >
-                  Ce champ est requis
-                </Help>
-              </Control>
-            </Field>
-            <Field>
-              <Control>
-                <Link
-                  className="button is-primary"
-                  to="/home"
-                  onClick={onSubmit}
-                >
-                  Se connecter
-                </Link>
-              </Control>
-            </Field>
-          </form>
-        </Box>
-      </Column>
-    </Columns>
+    <>
+      {authenticated ? (
+        <Redirect to="/home" />
+      ) : (
+        <Columns className="is-centered">
+          <Column className="is-narrow">
+            <Box style={{ marginTop: '1.5rem' }}>
+              <form>
+                <Field>
+                  <Label>Nom</Label>
+                  <Control>
+                    <Input
+                      placeholder="Votre nom"
+                      value={username}
+                      onChange={e => setUserName(e.target.value)}
+                      required
+                    />
+                    <Help
+                      color="danger"
+                      style={{ display: username ? 'none' : 'default' }}
+                    >
+                      Ce champ est requis
+                    </Help>
+                  </Control>
+                </Field>
+                <Field>
+                  <Label>Mot de passe</Label>
+                  <Control>
+                    <Input
+                      type="password"
+                      placeholder="Votre mot de passe"
+                      value={password}
+                      onChange={e => setPassword(e.target.value)}
+                      required
+                    />
+                    <Help
+                      color="danger"
+                      style={{ display: password ? 'none' : 'default' }}
+                    >
+                      Ce champ est requis
+                    </Help>
+                  </Control>
+                </Field>
+                <Field>
+                  <Control>
+                    <Button color="primary" onClick={onSubmit}>
+                      Se connecter
+                    </Button>
+                  </Control>
+                </Field>
+              </form>
+            </Box>
+          </Column>
+        </Columns>
+      )}
+    </>
   );
 };
 
 function extraProps(store) {
   return {
     login: store.login,
+    authenticated: store.session.authenticated,
   };
 }
 
