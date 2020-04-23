224eb89f72ebcd4cc54c2084856e13fdbcfb18b9
Tylosten <36881448+Tylosten@users.noreply.github.com>
2020-04-23 12:36:48 +0200

    Upload images to server

---
+++ src/server/routes/imgUpload.js
@@ -0,0 +1,42 @@
+import multer from 'multer';
+import sharp from 'sharp';
+import { v4 as uuid } from 'uuid';
+
+const upload_path = '/Images';
+const resizeMax = 300;
+
+const multerStorage = multer.memoryStorage();
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb('Please upload only images.', false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+  limits: {
+    fileSize: 4 * 1024 * 1024,
+  },
+});
+
+export const uploadImage = upload.single('file');
+
+export const resizeImage = async (req, res, next) => {
+  if (!req.file) {
+    throw 'File wasn\'t uploaded';
+  }
+
+  const filepath = `${upload_path}/${uuid()}.png`;
+  await sharp(req.file.buffer)
+    .resize(resizeMax, resizeMax, {
+      fit: sharp.fit.inside,
+      withoutEnlargement: true,
+    })
+    .toFile('public' + filepath);
+
+  req.filepath = filepath;
+  next();
+};
