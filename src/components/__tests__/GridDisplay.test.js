1eec3f25858499bdd2a43cf97a5b0d23802f4aa7
Tylosten <36881448+Tylosten@users.noreply.github.com>
2020-04-29 19:59:41 +0200

    Fix tests

--- src/components/__tests__/GridDisplay.test.js
+++ src/components/__tests__/GridDisplay.test.js
@@ -3,14 +3,38 @@ import { shallow } from 'enzyme';
 
 import { GridDisplay } from '../GridDisplay';
 
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'), // use actual for all non-hook parts
+  useLocation: () => ({
+    search: '',
+  }),
+  useHistory: () => ({
+    push: () => {},
+  }),
+}));
+
 describe('GridDisplay', () => {
   it('renders correctly', () => {
     const props = {
       grid: {
         id: 'G1',
         name: 'La grille de Mag',
-        lineThemes: ['T1', 'T2', 'T3', 'T4', 'T5', 'T6'],
-        colThemes: ['T7', 'T8', 'T9', 'T10', 'T11', 'T12'],
+        lineThemes: [
+          { id: 'T1' },
+          { id: 'T2' },
+          { id: 'T3' },
+          { id: 'T4' },
+          { id: 'T5' },
+          { id: 'T6' },
+        ],
+        colThemes: [
+          { id: 'T7' },
+          { id: 'T8' },
+          { id: 'T9' },
+          { id: 'T10' },
+          { id: 'T11' },
+          { id: 'T12' },
+        ],
       },
       users: [{ id: 'u1', name: 'Mag' }, { id: 'u2', name: 'Matt' }],
       edit: true,
